f6a3a3dac5fcb1a94eb74a6cab026f6c1dcfa0b9
Mateusz Dziewulski <m.dziewulski@gmail.com>
2013-11-10 12:23:45 +0100

    zajecia 10.11.13

---
+++ d1.c
@@ -0,0 +1,13 @@
+/*Program wczytuje liczbę całkowitą i za[isuje jej wartość do zmiennej "x"*/
+#include <stdio.h>
+int main() {
+   double x;
+
+   printf("Podaj liczbe rzeczywista: ");
+   scanf("%lf", &x);
+   printf("Wpisano licznbe rzeczywista: %.2lf\n", x);
+
+
+
+   return 0;
+}
